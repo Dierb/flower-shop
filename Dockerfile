43142eeeb3ae8e08845d8149e7cf8f6f034cbbbc
Ysak-Emir <emirchik.isak@gmail.com>
2022-12-12 15:00:45 +0300

    docker add

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM python:3.10.6
+FROM python:latest
 
 RUN mkdir /app
 WORKDIR /app/
--- main/settings.py
+++ main/settings.py
@@ -22,12 +22,15 @@ env = environ.Env()
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-0myrc$js-mgn%e^hxq81l_fl!bwpsf-!na4=9$*g*$l3c!vs9+'
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+SECRET_KEY = os.environ.get('SECRET_KEY')
+
+DEBUG = os.environ.get('DEBUG')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
